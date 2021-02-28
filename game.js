896a2b508e269e772764dfdb647e622f365c6588
davisgomes <davisgomes@g.ucla.edu>
2021-02-27 20:34:51 -0800

    Initialize tiny-graphics

---
+++ game.js
@@ -0,0 +1,53 @@
+import {defs, tiny} from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene
+} = tiny;
+
+export class Game extends Scene {
+    /**
+     *  **Base_scene** is a Scene that can be added to any display canvas.
+     *  Setup the shapes, materials, camera, and lighting here.
+     */
+    constructor() {
+        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
+        super();
+
+        // ** Shapes **
+        this.shapes = {
+            axis: new defs.Axis_Arrows()
+        }
+
+        // ** Materials **
+        this.materials = {
+            phong: new Material(new defs.Phong_Shader(), {
+               ambient: 0.5, color: hex_color("#ffffff"),
+            }),
+        }
+
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+    }
+
+    make_control_panel() {
+        // TODO:  Implement requirement #5 using a key_triggered_button that responds to the 'c' key.
+    }
+
+    display(context, program_state) {
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(Mat4.translation(0, 0, -10));
+        }
+
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, 1, 100);
+
+        const light_position = vec4(10, 10, 10, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+
+        let model_transform = Mat4.identity();
+
+        // TODO:  Draw the required boxes. Also update their stored matrices.
+        this.shapes.axis.draw(context, program_state, model_transform, this.materials.phong);
+    }
+}
